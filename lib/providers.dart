14fec6b68e434917f71b78a0088c5cb9923f4970
HtetAung <htetaung.ycc.pku@gmail.com>
2021-06-13 22:54:51 +0800

    add injection

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'injection_container.dart';
 import 'notifier/login_notifier.dart';
 
 
 final loginNotifierProvider = StateNotifierProvider(
-  (ref) => LoginNotifier(),
+  (ref) => LoginNotifier(loginUseCase: sl()),
 );
